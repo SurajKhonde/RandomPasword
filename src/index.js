d4e55b21940787f81d82207bb07ef86a17b144a9
surajdev9 <132157302+surajdev9@users.noreply.github.com>
2024-01-13 19:49:47 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { useState,useCallback, useEffect } from 'react';
+import ReactDOM from 'react-dom/client'
+import App from './App';
+const root = ReactDOM.createRoot(document.getElementById("Root"));
+root.render(<App/>)
+export default App
